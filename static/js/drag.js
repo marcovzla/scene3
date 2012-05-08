9579a5e7e18c4aadca3b444c0588739af51c0e32
Daniel Fried <dfried@email.arizona.edu>
2012-05-08 00:34:49 -0700

    keycommands to bind to objects, rudimentary selection rectangle

--- static/js/drag.js
+++ static/js/drag.js
@@ -12,9 +12,16 @@ var _currentResponseIndex = -1;
 var _sequence = 0;
 var _scene = 0;
 
-UNSELECTED_BORDER = "1px solid black";
-SELECTED_BORDER   = "1px solid yellow";
+var _selectionRect = false;
+var _selectionRectTop = -1;
+var _selectionRectLeft = -1;
+
+UNSELECTED_BORDER = "2px solid black";
+SELECTED_BORDER   = "2px solid yellow";
 UNBOUND_BACKGROUND = "rgb(240, 240, 240)";
+//SELECTION_RECT = "rgb(240, 240, 240)"
+//SELECTION_RECT = "#FF0000";
+SELECTION_RECT = "yellow";
 
 enable_user_input();
 
@@ -44,20 +51,47 @@ function disable_user_input()
 
 function OnKeyUp(e)
 {
-    if (e.keycode == 46) // delete
+    console.log(e);
+    if (e.keyCode == 46) // delete
     {
-        var word_list = _responses[_currentResponseIndex].word_list;
-        for (var i = 0; i < word_list.length; i++) {
-            if (_wordsSelectedMask[i])
-            {
-                delete word_list[i].object_binding
-            };
-        };
-
+        assign_selected(-1);
+        unselect_all_words();
+        update_div_colors();
     }
-
-    update_div_colors();
+    else if (e.keyCode >= 48 && e.keyCode <= 57) // number
+    {
+        var numeral = e.keyCode - 48;
+        if (room.objects.length > numeral) {
+            assign_selected(numeral);
+        }
+        unselect_all_words();
+        update_div_colors();
+    }
+    else if (e.keyCode == 79) // 'o', for last object
+    {
+        if (room.objects.length > 0) {
+            assign_selected(room.objects.length - 1);
+        }
+        unselect_all_words();
+        update_div_colors();
+    }
     return true;
+}
+
+function assign_selected(assignment_index) {
+    /*set object_binding for selected words to assignment_index, if assignment_index >= 0
+    if assignment_index = -1, will clear object_binding for selected words*/
+    var word_list = _responses[_currentResponseIndex].word_list;
+    for (var i = 0; i < word_list.length; i++) 
+    {
+        if (_wordsSelectedMask[i])
+        {
+            if (assignment_index > -1) 
+                word_list[i].object_binding = assignment_index;
+            else
+                delete word_list[i].object_binding
+        };
+    };
 }
 
 function OnMouseDown(e)
@@ -70,7 +104,7 @@ function OnMouseDown(e)
     var target = e.target != null ? e.target : e.srcElement;
 
     var word_list = _responses[_currentResponseIndex].word_list;
-    
+
     // for IE, left click == 1
     // for Firefox, left click == 0
     if (e.shiftKey) 
@@ -118,6 +152,10 @@ function OnMouseDown(e)
         
         // we need to access the element in OnMouseMove
         //_dragElement = target;
+    } else { // selection rect
+        _selectionRect = true;
+        _selectionRectTop = e.pageY;
+        _selectionRectLeft = e.pageX;
     }
 
     // tell our code to start moving the element with the mouse
@@ -140,6 +178,24 @@ function OnMouseMove(e)
     if (e == null) 
         var e = window.event; 
 
+    if (_selectionRect)
+    {
+        var c=$("#canvas");
+        var ctx=c[0].getContext("2d");
+        var top1 = _selectionRectTop - c.offset().top;
+        var left1 = _selectionRectLeft - c.offset().left;
+        var top2 = e.pageY - c.offset().top;
+        var left2 = e.pageX - c.offset().left;
+        ctx.fillStyle = SELECTION_RECT;
+        ctx.clearRect(0,
+                      0,
+                      c.width(),
+                      c.height());
+        ctx.fillRect(Math.min(left1, left2),
+                     Math.min(top1, top2),
+                     Math.abs(left1 - left2),
+                     Math.abs(top1 - top2));
+    }
     for (var i = 0; i < _dragElements.length; i++) {
         var init_offset = _dragElementsOffsets[i];
         var new_pos = {};
@@ -163,6 +219,25 @@ function show_dialog(str)
 
 function OnMouseUp(e)
 {
+    if (_selectionRect) {
+        console.log("here");
+        _selectionRect = false;
+        if (e.pageX != _selectionRectLeft && e.pageY != _selectionRectTop) {
+            select_divs_within_rect(e.pageX, e.pageY, 
+                                    _selectionRectLeft, _selectionRectTop);
+        }
+        var c=$("#canvas");
+        var ctx=c[0].getContext("2d");
+        var top1 = _selectionRectTop - c.offset().top;
+        var left1 = _selectionRectLeft - c.offset().left;
+        var top2 = e.pageY - c.offset().top;
+        var left2 = e.pageX - c.offset().left;
+        ctx.fillStyle = SELECTION_RECT;
+        ctx.clearRect(0,
+                      0,
+                      c.width(),
+                      c.height());
+    }
     if (_dragElements.length > 0)
     {
         //_dragElement.style.zIndex = _oldZIndex;
@@ -176,28 +251,19 @@ function OnMouseUp(e)
             var idx = lst[0];
             var shape = lst[1];
             var color = lst[2];
-            for (var i = 0; i < word_list.length; i++) {
-                if (_wordsSelectedMask[i]) 
-                {
-                    //$(_words[i].div).hide();
-                    word_list[i].object_binding = idx
-                }
-            };
-        } else {
-            for (var i = 0; i < word_list.length; i++) {
-                if (_wordsSelectedMask[i]) 
-                {
-                    delete word_list[i].object_binding;
-                }
-            };
+            // assign object_bindings
+            assign_selected(idx);
+        } else 
+        {
+            // clear bindings
+            assign_selected(-1);
         }
         // repos drag elements
         for (var i = 0; i < _dragElements.length; i++) {
             // stupid hack, what is going on with offset?
-            for (var j = 0; j < 4; j++) {
-                $(_dragElements[i]).offset(_dragElementsOffsets[i]);
-            };
-            $(_dragElements[i]).css("border", UNSELECTED_BORDER);
+            //for (var j = 0; j < 4; j++) {
+            $(_dragElements[i]).offset(_dragElementsOffsets[i]);
+            //};
         };
 
         // do colors
@@ -208,13 +274,22 @@ function OnMouseUp(e)
         _dragElementsOffsets = [];
 
         // unselect words
-        for (var i = 0; i < _wordsSelectedMask.length; i++) {
-            _wordsSelectedMask[i] = false;
-        };
+        unselect_all_words();
         // we're done with these events until the next OnMouseDown
         document.onmousemove = null;
         document.onselectstart = null;
     }
+}
+
+function unselect_all_words() {
+    var word_list = _responses[_currentResponseIndex].word_list;
+    for (var i = 0; i < _wordsSelectedMask.length; i++) {
+        if (_wordsSelectedMask[i])
+        {
+            $(word_list[i].div).css("border", UNSELECTED_BORDER);
+            _wordsSelectedMask[i] = false;
+        };
+    };
 }
 
 function ExtractNumber(value)
@@ -305,6 +380,26 @@ function previous_response()
     load_response_index(_currentResponseIndex - 1)
 }
 
+function select_divs_within_rect(corner1x, corner1y, corner2x, corner2y)
+{
+    var x0 = Math.min(corner1x, corner2x);
+    var x1 = Math.max(corner1x, corner2x);
+    var y0 = Math.min(corner1y, corner2y);
+    var y1 = Math.max(corner1y, corner2y);
+    console.log("selecting within " + x0 + ", " + x1 + ", " + y0 + ", " + y1);
+
+    word_list = _responses[_currentResponseIndex].word_list;
+    for (var i = 0; i < word_list.length; i++) {
+        var div = word_list[i].div;
+        var left = $(div).position().left;
+        var top = $(div).position().top;
+        var right = $(div).width() + left;
+        var bottom = $(div).height() + top;
+        _wordsSelectedMask[i] = (left >= x0 && top >= y0 && right <= x1 && bottom <= x2);
+        $(word_list[i].div).css("border", _wordsSelectedMask[i] ? SELECTED_BORDER : UNSELECTED_BORDER);
+    };
+}
+
 function update_div_colors()
 {
     word_list = _responses[_currentResponseIndex].word_list;
@@ -325,3 +420,14 @@ function update_div_colors()
         }
     };
 }
+
+function draw() {  
+    var canvas = document.getElementById("canvas");  
+    var ctx = canvas.getContext("2d");  
+
+    ctx.fillStyle = "rgb(200,0,0)";  
+    ctx.fillRect (10, 10, 55, 50);  
+
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";  
+    ctx.fillRect (30, 30, 55, 50);  
+}  
